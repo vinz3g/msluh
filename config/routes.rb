7cac213c5d27d7d47adbd5aa4b1b1b025c20d94b
vinz3g <lvonti51@gmail.com>
2016-04-12 19:44:43 -0400

    salvando datos en controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :articles
+  
 get 'welcome/index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-resources :articles
+    resources :articles
   # You can have the root of your site routed with "root"
  
    root 'welcome#index'
